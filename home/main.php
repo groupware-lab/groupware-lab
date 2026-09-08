789fafdf7ecdbe8dcf0c19163151627eb1128576
kbekura111 <bekura0706@gmail.com>
2026-09-08 11:49:46 +0900

    20260908更新

--- home/main.php
+++ home/main.php
@@ -41,7 +41,7 @@ if(!isset($_SESSION['user_id'])) {
         <!-- デスクトップ以外はリンクでページ遷移するため -->
         <div class="bg-white border-bottom py-2 px-4 mb-4">
             <div class="container-fluid p-0 d-flex flex-wrap gap-2">
-                <ul>
+                <ul class="">
                     <li><a href="#" class="btn btn-outline-secondary btn-sm">デスクトップ</a></li>
                     <li><a href="#" class="btn btn-outline-secondary btn-sm">出退勤管理</a></li>
                     <li><a href="#" class="btn btn-outline-secondary btn-sm">社内掲示板</a></li>
--- test.php
+++ test.php
@@ -4,5 +4,6 @@ $hash = password_hash($password, PASSWORD_DEFAULT);
 echo $hash;
 
 ?>
-//パスワードのハッシュ化のテスト用のphp
-//使うときはpasswordの変数の値を変えてデバックをする
+
+<!-- パスワードのハッシュ化のテスト用のphp
+使うときはpasswordの変数の値を変えてデバックをする -->
